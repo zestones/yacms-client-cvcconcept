906ecbce7345fc830c4c36ae5db37ad26af53909
Idriss <75535295+zestones@users.noreply.github.com>
2026-05-01 10:27:46 +0200

    Initialize draft

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import sitemap from '@astrojs/sitemap';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    site: 'https://zestones.github.io',
+    base: '/yacms-client-template',
+    integrations: [react(), sitemap()],
+    build: {
+        inlineStylesheets: 'always',
+        compressHTML: true
+    },
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                'zod-to-json-schema': path.resolve(__dirname, './src/stubs/zod-to-json-schema.ts'),
+            },
+        },
+        css: {
+            transformer: 'postcss',
+        },
+        server: {
+            host: true,
+            watch: {
+                ignored: ['**/yablocks/**']
+            }
+        },
+        ssr: {
+            noExternal: ['lucide-react'],
+        },
+    },
+});
